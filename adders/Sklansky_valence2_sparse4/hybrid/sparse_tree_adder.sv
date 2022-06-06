aa8304d374bba8019cb864bc5003ffc0dd463f97
Walter Ruggeri <walter.ruggeri.95@gmail.com>
2022-06-06 23:52:48 +0200

    Add files via upload

--- adders/Sklansky_valence2_sparse4/hybrid/sparse_tree_adder.sv
+++ adders/Sklansky_valence2_sparse4/hybrid/sparse_tree_adder.sv
@@ -4,6 +4,7 @@ Author:         Walter Ruggeri
 Description:    4-sparse tree adder
 
 14.04.2022      Initial release
+06.06.2022      Corrected formatting
 */
 
 
@@ -13,7 +14,7 @@ module sparse_tree_adder
 )
 (
     input [N_BIT - 1 : 0] operand_1, operand_2,
-	input carry_in,
+    input carry_in,
     output [N_BIT - 1 : 0] sum,
     output carry_out, overflow
 );
